434f761203d0b0891f3a762c9e0937e6ff51093a
MATEUS BISPO RAMOS <21192976@CFP127ALUNO.LOCAL>
2021-09-24 09:14:52 -0300

    Correção do bug Login

--- processa_login.php
+++ processa_login.php
@@ -3,13 +3,14 @@
     session_start();
     
     require_once("./funcoes.php");
+    
 
     if(isset($_POST["txt_usuario"]) || isset($_POST["txt_senha"])){
 
         $usuario = $_POST["txt_usuario"];
         $senha = $_POST["txt_senha"];
 
-        realizarLogin($usuario, $senha, lerArquivo("./dados.usuarios.json"));   
+        realizarLogin($usuario, $senha, lerArquivo("./dados/usuarios.json"));   
         
     } elseif($_GET["logout"]){
         finalizarLogin();
